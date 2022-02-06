05a4a39585250f405654c089927e458ae1bbfe51
alex maslakov <alex@serendipia.email>
2022-02-06 15:08:28 -0300

    wip

--- deploy/Zap.js
+++ deploy/Zap.js
@@ -1,6 +1,5 @@
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy } = deployments;
-
   const { deployer } = await getNamedAccounts();
 
   await deploy("Zap", {
@@ -13,7 +12,15 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   const zap = await ethers.getContract("Zap");
   const joe = await deployments.get("JoeToken");
   const router = await deployments.get("JoeRouter02");
-  await zap.initialize(joe.address, router.address);
+
+  // this may be called only once, during the 1st deployment
+  // may not be called a 2nd time; otherwise, will cause an error
+  try {
+    await zap.initialize(joe.address, router.address);
+  } catch(err) {
+    console.log("Error in `zap.initialize(...)`; no worries - it could've been initilized already;")
+    console.log(`details: ${err}\r\n\r\n`)
+  }
 };
 
 module.exports.tags = ["Zap"];
