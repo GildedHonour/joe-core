bc8e0b7444e30252a230bb7d5f324949c4d75aa8
alex maslakov <alex@serendipia.email>
2022-02-08 23:30:44 -0300

    wip

--- deploy/FarmLens.js
+++ deploy/FarmLens.js
@@ -17,18 +17,25 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   } else {
     throw Error("No WAVAX!");
   }
+
+  //TODO: for the chain 43114
+  //the names of the variables of 'wavaxUsdcAddress' and 'wavaxDaiAddress' have been confused
+  //because in reality
+  //wavaxUsdcAddress is JoePair of WAVAX-DAI
+  //whereas
+  //wavaxDaiAddress is JoePair of  WAVAX-USDC
   if (chainId === "43114") {
-    wavaxUsdtAddress = address("0xeD8CBD9F0cE3C6986b22002F03c6475CEb7a6256");
-    wavaxUsdcAddress = address("0x87Dee1cC9FFd464B79e058ba20387c1984aed86a");
-    wavaxDaiAddress = address("0xA389f9430876455C36478DeEa9769B7Ca4E3DDB1");
+    wavaxUsdtAddress = "0xeD8CBD9F0cE3C6986b22002F03c6475CEb7a6256"; //JoePair of WAVAX-USDT
+    wavaxUsdcAddress = "0x87Dee1cC9FFd464B79e058ba20387c1984aed86a"; //JoePair of WAVAX-USDC
+    wavaxDaiAddress = "0xA389f9430876455C36478DeEa9769B7Ca4E3DDB1";  //JoePair of WAVAX-DAI
   } else if (chainId === "4") {
-    wavaxUsdtAddress = address("0x63fce17ba68c82a322fdd5a4d03aedbedbd730fd");
-    wavaxUsdcAddress = address("0x63fce17ba68c82a322fdd5a4d03aedbedbd730fd");
-    wavaxDaiAddress = address("0x63fce17ba68c82a322fdd5a4d03aedbedbd730fd");
+    wavaxUsdtAddress = "0x63fce17ba68c82a322fdd5a4d03aedbedbd730fd";
+    wavaxUsdcAddress = "0x63fce17ba68c82a322fdd5a4d03aedbedbd730fd";
+    wavaxDaiAddress = "0x63fce17ba68c82a322fdd5a4d03aedbedbd730fd";
   } else if (chainId === "43113") { //Avalanche > Fuji
-    wavaxUsdtAddress = address("???");
-    wavaxUsdcAddress = address("??");
-    wavaxDaiAddress = address("??");
+    wavaxUsdtAddress = "0x5528f1355c070682a2B079a5dCA8129F568271Af";
+    wavaxUsdcAddress = "0x5bc94145def1d0034fa08d64eff59e72b3a4f41d";
+    wavaxDaiAddress = "0xCD4CF44B29cC8150F67A6447929C7B9f89d51827";
   }
 
   const joeAddress = (await deployments.get("JoeToken")).address;
